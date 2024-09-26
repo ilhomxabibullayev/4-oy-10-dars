78cdb034358c3383db56b416b2948d41df6a0963
ilhomxabibullayev <ilhomxabibullayev2004@gmail.com>
2024-09-26 22:20:09 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// 1 masala
+
+let getAvarage = ([1, 2, 3]);
+
+// 2 masala
+
+let invert = (array) => {
+    let newarr = []
+    for (let i = 0; i <= array.length; i++) {
+        array[i] === 0 ? newarr.push(array[i]) : newarr.push(-array[i])
+    }
+
+    return newarr;
+}
+
+let result = invert ([1, 2, 3])
+let result2 = invert ([-1, 2, -3, 0, -5])
+
+console.log(result)
+console.log(result2)
+
+// 3 masala
+
+let getbonus = (n) => {
+    if (n === true) {
+        return '5000'
+    } else {
+        return '1000'
+    }
+}
+
+let t = getbonus (true);
+let f = getbonus (false);
+
+console.log(t, f)
+
+// 5 masala
+
+let enough = (a, b, c) => {
+    return a - (b + c)
+}
+
+let result3 = enough(10, 5, 5);
+
+console.log(result3);
+
+let enough2 = (a, b, c) => {
+    return a - (b + c)
+}
+
+let result4 = enough2(12, 5, 10);
+
+console.log(-result4);
